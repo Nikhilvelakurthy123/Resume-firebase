c4cfde78ed5330b9de52b26dee97ad9e7608e5b5
Nikhil <bunnynik6@gmail.com>
2021-09-11 06:39:14 +0530

    weather component removed

--- src/App.js
+++ src/App.js
@@ -17,64 +17,64 @@ function App() {
   const engineeringImage = "https://4.bp.blogspot.com/-MJf6pZABTPY/Wju-yqeuU-I/AAAAAAAAArE/pkyJJBqPdgsLVnvsxyn1xtJytrqCRH8KgCLcBGAs/s1600/logo%2Bnew.jpg"
   const userPicture = "/Nikhil picture.jpg"
   return (
-    <div className="container-fluid w-75 resume-block p-4 mb-4" >
-      <div className="card" >
+    <div className="container-fluid w-75 resume-block p-4 mb-4 " >
+      <div className="card border-0" >
         <div style={{ overflow: "hidden" }}>
           <img src={userPicture} alt="User" style={{ padding: "20px", marginLeft: "20px", height: "250px", width: "180px", float: "left" }} />
           <h5 className="name p-2" style={{ float: "right", display: "block" }} >Velakurthy Nikhil</h5>
         </div>
-        <div className="card-body">
+        <div className="card-body border-0" >
 
-          <div className="card row mt-2 ">
-            <div className="card col">
+          <div className="card row mt-2 border-0">
+            <div className="card col border-0">
               <h5 className="card-title">PROFILE SUMMARY:</h5>
               <p className="card-body">
-                <ul className="list-group">
-                  <li className="list-group-item">Over 2.2  years of IT experience in Object Oriented Programming Testing,
+                <ul className="list-group ">
+                  <li className="list-group-item border-0">Over 2.2  years of IT experience in Object Oriented Programming Testing,
                     and Implementation of Client - Server, Distributed Multi-tiered and Large Scale Enterprise
                     applications in Waterfall  methodology using JAVA and JEE technologies.</li>
-                  <li className="list-group-item">Expertise in Java programming and have a good understanding on OOPs, Collections,
+                  <li className="list-group-item border-0">Expertise in Java programming and have a good understanding on OOPs, Collections,
                     Exceptions Handling,  Multi-Threading and Lambda Expressions.</li>
-                  <li className="list-group-item">Experience in understanding and implementing Design Patterns such as Singleton, Prototype,
+                  <li className="list-group-item border-0">Experience in understanding and implementing Design Patterns such as Singleton, Prototype,
                     Factory etc.</li>
-                  <li className="list-group-item">Experience in developing Web and Enterprise applications using technologies JDBC, JSP and
+                  <li className="list-group-item border-0">Experience in developing Web and Enterprise applications using technologies JDBC, JSP and
                     Java Persistence API (JPA).</li>
-                  <li className="list-group-item">Experience in Developing Application using frameworks Spring Dependency Injection (DI),
+                  <li className="list-group-item border-0">Experience in Developing Application using frameworks Spring Dependency Injection (DI),
                     IOC, MVC, Boot .</li>
-                  <li className="list-group-item">Experience in building flexible, reliable, efficient and secured Enterprise and Web-based
+                  <li className="list-group-item border-0">Experience in building flexible, reliable, efficient and secured Enterprise and Web-based
                     applications using Spring MVC and Spring BOOT Web Flow frameworks.</li>
-                  <li className="list-group-item">Experience in writing SQL queries in Relational Database management systems like Oracle, MySQL.</li>
-                  <li className="list-group-item">Expertise in using Version control Systems likes Git</li>
-                  <li className="list-group-item">Having very strong interpersonal skills and ability to work independently and within the group,
+                  <li className="list-group-item border-0">Experience in writing SQL queries in Relational Database management systems like Oracle, MySQL.</li>
+                  <li className="list-group-item border-0">Expertise in using Version control Systems likes Git</li>
+                  <li className="list-group-item border-0">Having very strong interpersonal skills and ability to work independently and within the group,
                     can learn quickly and easily adaptable to the working environment.</li>
-                  <li className="list-group-item">Good exposure in interacting with clients and solving application environment issues and can
+                  <li className="list-group-item border-0">Good exposure in interacting with clients and solving application environment issues and can
                     communicate effectively with people at different levels including stakeholders, internal teams, and
                     the senior management.</li>
                 </ul>
               </p>
             </div>
 
-            <div className="card mt-2">
+            <div className="card mt-2 border-0">
               <h5 className="card-title">TECHNICAL SKILLS:</h5>
               <p className="card-body">
                 <ul className="list-group">
-                  <li className="list-group-item mb-1"><b>Languages	:</b>	<span className="float-right">Java  and SQL</span></li>
-                  <li className="list-group-item mb-1"><b>Java/JEE Technologies	:</b>	<span className="float-right">Java EE, JDBC, JPA</span></li>
-                  <li className="list-group-item mb-1"><b>Web Technologies	:</b>	<span className="float-right">HTML5, CSS3, JavaScript,</span></li>
-                  <li className="list-group-item mb-1"><b>Web Services	:</b>	<span className="float-right">Restful Web Services</span></li>
-                  <li className="list-group-item mb-1"><b>Frameworks	:</b>	<span className="float-right">Spring MVC, Spring Boot</span></li>
-                  <li className="list-group-item mb-1"><b>RDBMS	:</b>	<span className="float-right">ORACLE 11g, MySQL</span></li>
-                  <li className="list-group-item mb-1"><b>Web/Application Servers	:</b>	<span className="float-right">Apache Tomcat</span></li>
-                  <li className="list-group-item mb-1"><b>IDE s	:</b>	<span className="float-right">STS, Eclipse</span ></li>
-                  <li className="list-group-item mb-1"><b>Design Patterns	:</b>	<span className="float-right">Singleton, Factory, Prototype Version Controls	:	GIT</span></li>
+                  <li className="list-group-item mb-1 border-0"><b>Languages	:</b>	<span className="float-right">Java  and SQL</span></li>
+                  <li className="list-group-item mb-1 border-0"><b>Java/JEE Technologies	:</b>	<span className="float-right">Java EE, JDBC, JPA</span></li>
+                  <li className="list-group-item mb-1 border-0"><b>Web Technologies	:</b>	<span className="float-right">HTML5, CSS3, JavaScript,</span></li>
+                  <li className="list-group-item mb-1 border-0"><b>Web Services	:</b>	<span className="float-right">Restful Web Services</span></li>
+                  <li className="list-group-item mb-1 border-0"><b>Frameworks	:</b>	<span className="float-right">Spring MVC, Spring Boot</span></li>
+                  <li className="list-group-item mb-1 border-0"><b>RDBMS	:</b>	<span className="float-right">ORACLE 11g, MySQL</span></li>
+                  <li className="list-group-item mb-1 border-0"><b>Web/Application Servers	:</b>	<span className="float-right">Apache Tomcat</span></li>
+                  <li className="list-group-item mb-1 border-0"><b>IDE s	:</b>	<span className="float-right">STS, Eclipse</span ></li>
+                  <li className="list-group-item mb-1 border-0"><b>Design Patterns	:</b>	<span className="float-right">Singleton, Factory, Prototype Version Controls	:	GIT</span></li>
                 </ul>
               </p>
             </div>
 
-            <div className="card mt-2">
+            <div className="card mt-2 border-0">
               <h5 className="card-title">PROFESSIONAL EXPERIENCE:</h5>
               <p className="card-body">
-                <div className="card w-50 border-0" style={{}}>
+                <div className="card w-50 p-2 ms-4 border-0" style={{}}>
                   <dl>
                     <div>
                       <dt style={{ float: "left" }}>Project Name  :</dt>
@@ -89,14 +89,14 @@ function App() {
                 <hr />
                 <span style={{ float: "center" }} className="mb-2 mt-1 p-2"><b>Responsibilities:</b></span>
                 <ul className="list-group">
-                  <li className="list-group-item mb-1">Participated in Waterfall methodology and involved in Design, development, Implementation, and testing of the enterprise applications.</li>
-                  <li className="list-group-item mb-1">Used Hibernate, Object/relational mapping (ORM) solution, the technique of mapping data representation from MVC model to Oracle relational data model with a SQL-based schema.</li>
-                  <li className="list-group-item mb-1">Created and injected Spring services, Spring controllers, and DAOs to achieve dependency injection and to wire objects of business classes.</li>
-                  <li className="list-group-item mb-1">Worked extensively on developing controllers, Interfaces and implementation classes using Spring MVC framework & used Spring Boot based web service through REST.</li>
-                  <li className="list-group-item mb-1">Developed, debugged, integrated and deployed applications using STS and used GIT as a Version Control.</li>
-                  <li className="list-group-item mb-1">Used Log4j for tracking errors and debugging the code and Resolved bugs and Change Requests to already existing code.</li>
+                  <li className="list-group-item mb-1 border-0">Participated in Waterfall methodology and involved in Design, development, Implementation, and testing of the enterprise applications.</li>
+                  <li className="list-group-item mb-1 border-0">Used Hibernate, Object/relational mapping (ORM) solution, the technique of mapping data representation from MVC model to Oracle relational data model with a SQL-based schema.</li>
+                  <li className="list-group-item mb-1 border-0">Created and injected Spring services, Spring controllers, and DAOs to achieve dependency injection and to wire objects of business classes.</li>
+                  <li className="list-group-item mb-1 border-0">Worked extensively on developing controllers, Interfaces and implementation classes using Spring MVC framework & used Spring Boot based web service through REST.</li>
+                  <li className="list-group-item mb-1 border-0">Developed, debugged, integrated and deployed applications using STS and used GIT as a Version Control.</li>
+                  <li className="list-group-item mb-1 border-0">Used Log4j for tracking errors and debugging the code and Resolved bugs and Change Requests to already existing code.</li>
                 </ul>
-                <div className="card">
+                <div className="card border-light">
                   <p className="card-body">
                     <dl>
                       <dt>Environment:</dt>
@@ -112,8 +112,8 @@ function App() {
             </div>
 
             <div className="row">
-              <div className="card col w-25">
-                <img className="card-img-top" src={schoolImage} alt="School" />
+              <div className="card col w-25 border-light" >
+                <img className="card-img-top" src={schoolImage} alt="School" style={{ width: "75%", height: "200px" }} />
                 <div className="card-body">
                   <h5 className="card-title">Naagarjuna Model School</h5>
                   <dl>
@@ -128,8 +128,8 @@ function App() {
                   </dl>
                 </div>
               </div>
-              <div className="card col w-25">
-                <img className="card-img-top" src={interImage} alt="College" />
+              <div className="card col w-25 border-light" >
+                <img className="card-img-top" src={interImage} alt="College" style={{ width: "75%", height: "200px" }} />
                 <div className="card-body">
                   <h5 className="card-title">Narayana Junior College</h5>
                   <dl>
@@ -146,8 +146,8 @@ function App() {
                   </dl>
                 </div>
               </div>
-              <div className="card col w-25">
-                <img className="card-img-top" src={engineeringImage} alt="Engineering" />
+              <div className="card col w-25 border-light" >
+                <img className="card-img-top" src={engineeringImage} alt="Engineering" style={{ width: "75%", height: "200px" }} />
                 <div className="card-body">
                   <h5 className="card-title">Sphoorthy Engineering College</h5>
                   <dl>
@@ -159,48 +159,22 @@ function App() {
                     <dd>66.8</dd>
                   </dl>
                 </div>
-
               </div>
             </div>
-
-
 
             <div>
-              <div className="row">
-                <h4>React Samples</h4>
-                <div className="d-flex">
-                  <div className="card col">
-
-                    <button className="card-title btn btn-primary">
-                      <Link to="/expenseTracker" className="a">ExpenseTrackerComponent</Link>
-                    </button>
-                  </div>
-                  <div className="card col">
-                    <button className="card-title btn btn-primary">
-                      <Link to="/calc" className="a">Calculator</Link>
-                    </button>
-                  </div>
-                  <div className="card col">
-                    <button className="card-title btn btn-primary">
-                      <Link to="/TictactoeComponent" className="a">Tictactoe</Link>
-                    </button>
-                  </div>
-
+              <div className="card border-light" >
+                <h4 className="card-title"> React Examples </h4>
+                <div className="card-body">
+                  <ul>
+                    <li><Link to="/expenseTracker" className="a">ExpenseTrackerComponent</Link></li>
+                    <li><Link to="/calc" className="a">Calculator</Link></li>
+                    <li><Link to="/TictactoeComponent" className="a">Tictactoe</Link></li>
+                    {/* <li><Link to="/weatherapp" className="a">Weather App</Link></li> */}
+                  </ul>
                 </div>
               </div>
-              {/* <div className="row">
-                <div className="d-flex">
-                  <div className="card col">
-                    <button className="card-title btn btn-primary">
-                      <Link to="/weatherapp" className="a">Weather App</Link>
-                    </button>
-                  </div>
-                </div>
-              </div> */}
-
             </div>
-
-
           </div>
         </div>
 
