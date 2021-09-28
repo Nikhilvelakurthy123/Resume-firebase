8466d0e76e179f27746ecf83ad1e3c13950c8ba5
Nikhil <bunnynik6@gmail.com>
2021-09-28 06:52:05 +0530

    shopping cart

--- src/ShoppingAPI/Components/ShoppingHome.js
+++ src/ShoppingAPI/Components/ShoppingHome.js
@@ -22,10 +22,10 @@ function ShoppingHome() {
         <div className="container-fluid mb-4">
             {/* <div><pre>{JSON.stringify(getAllProducts, null, 2)}</pre></div> */}
             <div className="col-12 ">
-                <div className='d-flex flex-row flex-wrap mb-4 p-4' style={{ height: ' 600px ', overflow: 'auto' }}>
+                <div className='d-flex flex-row flex-wrap mb-4 p-4' style={{ height: ' inherit ', overflow: 'auto' }}>
                     {
                         getAllProducts.map(data =>
-                            <div className="card ms-2 me-2 mt-2" key={data.id} style={{ height: "350px", width: "200px" }}>
+                            <div className="card ms-2 me-2 mt-2" key={data.id} style={{ height: "350px", width: "300px" }}>
                                 <img src={data.image} alt="Image not Available" style={{ height: "100px", width: "100px", alignSelf: "center" }} />
                                 <p className="card-title ms-1 mt-4" style={{ fontSize: "1.25rem" }}>{data.title.length < 70 ? data.title : (data.title).substring(0, 70).concat(".....")}</p>
                                 <p className="card-footer position-absolute bottom-0 " style={{ width: "inherit" }}>
